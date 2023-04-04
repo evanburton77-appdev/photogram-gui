0a4b1ce870040bb54e91e2de1fc44e4d241a53fd
Evan Burton <evanburton77@gmail.com>
2023-04-04 21:17:08 +0000

    delete feature added

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,4 +5,25 @@ class PhotosController < ApplicationController
 
     render({ :template => "photo_templates/index.html.erb" })
   end
+
+  def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id })
+
+    @the_photo = matching_photos.first
+
+    render({ :template => "photo_templates/show.html.erb" })
+  end
+
+  def delete
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({ :id => the_id })
+
+    the_photo = matching_photos.first
+
+    the_photo.destroy
+    redirect_to("/photos")
+    # render({ :template => "photo_templates/delete.html.erb" })
+  end
 end
